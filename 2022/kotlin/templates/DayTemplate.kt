8f11aff2e81adf342f1c940d6e8d31edc42869ca
Gøran Frost <goranfr@volue.com>
2023-09-06 11:15:29 +0200

    fix template with resource/data changes

--- 2022/kotlin/templates/DayTemplate.kt
+++ 2022/kotlin/templates/DayTemplate.kt
@@ -1,12 +1,14 @@
-class DayDAY_NUMBER(override val example: Boolean = false) : Day {
+import common.Resource
+
+class DayDAY_NUMBER(override val isExample: Boolean = false) : Day {
     override val inputFile: String = "DayDAY_NUMBER.txt"
     override fun part1() : Int {
-        val data = Resource.asList(data())
+        val data = Resource.asList(data)
         TODO("Not Implemented")
     }
 
     override fun part2() : Int {
-        val data = Resource.asList(data())
+        val data = Resource.asList(data)
         TODO("Not Implemented")
     }
 }
